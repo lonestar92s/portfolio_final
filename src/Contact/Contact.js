0d6ce15b7511e75faa711156a8466416d8f53f96
Andrew Aluko <aluko17@icloud.com>
2019-03-01 14:13:46 -0600

    initial commit

---
+++ src/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+import { Icon } from 'semantic-ui-react'
+
+export default class Contact extends Component{
+	render() {
+		return (
+		<div className="Main">
+		<h1>Contact Info</h1>
+		<Icon name="github" />
+		</div>
+
+		)
+	}
+}
---
+++ src/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../App.css';
+import { Card, Button } from 'react-bootstrap'
+
+
+export default class Home extends Component{
+	render() {
+		return (
+		<div className="Main">
+		
+		<h1>Creative. Capable. Analytical.</h1>
+
+		</div>
+
+		)
+	}
+}

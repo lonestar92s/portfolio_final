ea8a6f3a8b1d20dd5214573ef94582cd592902e4
Andrew Aluko <aluko17@icloud.com>
2019-03-04 16:49:37 -0600

    color changes

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import '../App.css';
 import { Icon } from 'semantic-ui-react'
 
+
 export default class Contact extends Component{
 	render() {
 		return (
+		<div>
 		<div className="Main">
 		<h1>Contact Info</h1>
-		<Icon name="github" />
+		<a href='#'><Icon name="github" /></a>
+		</div>
 		</div>
 
 		)
